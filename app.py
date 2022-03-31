9124b07966982bf1928515a90adce0368d86bb94
Veloxization <github@velo.anonaddy.com>
2022-03-31 22:29:49 +0300

    Added login and signup pages, new links for login and signup POSTs

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ db = SQLAlchemy(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
