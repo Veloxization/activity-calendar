e2a4eb6cc39919f4f99894797525319c00eb30e8
Veloxization <github@velo.anonaddy.com>
2022-04-23 16:38:34 +0300

    Disabled the log in button until valid length username and password are entered

--- static/scripts/loginscript.js
+++ static/scripts/loginscript.js
@@ -2,5 +2,35 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 const username = urlParams.get('username');
+const usernameField = document.getElementById('username');
+const passwordField = document.getElementById('password');
+const submitButton = document.getElementById('submit-button');
+
+var usernameValid = false;
+var passwordValid = false;
 
 if (username != null) document.getElementById('username').value = username;
+
+function checkForm() {
+    usernameValid = checkUsername();
+    passwordValid = checkPassword();
+    activateButton();
+}
+
+function checkUsername() {
+    var username = usernameField.value;
+    return username.length >= 3 && username.length <= 20;
+}
+
+function checkPassword() {
+    var password = passwordField.value;
+    return password.length >= 8 && password.length <= 64
+}
+
+function activateButton() {
+    if (usernameValid && passwordValid) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+}
